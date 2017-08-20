e4d2e7e05ca137664584d5a59060b457f1fca1fb
hiruna72 <hiruna72@gmail.com>
2017-08-20 15:18:51 +0530

    configured my database to use. change the database to yours

--- signin.php
+++ signin.php
@@ -4,11 +4,39 @@
 
 	<?php            
             
-            // ========== make the link to db and add the query here ! ========================
+            // ========== make the link to db and add the query here ! 
+            // ================ change the db =============================
+            $database= 'co226project';
+            $db = mysqli_connect('localhost','root','',$database)
+            or die('Error connecting to MySQL server.');
+    
+            // ================ query =============================
+            $table1 ='serviceStationInfo';
+
     
             // if user name and passwrd match do qeury to update info
+            $userName=$_GET["user-name"];
+            $paswd=$_GET["acc-password"];
+            
+            $query = "SELECT user_name,acc_password FROM $table1 WHERE user_name = '".$userName."' && acc_password = '".$paswd."'   ";
     
-            // else echo err
+            mysqli_query($db, $query) or die ('Error querying the DB.');
+	        $result = mysqli_query($db, $query);
+    
+            //note the difference between mysql and mysqli
+    
+            if (mysqli_num_rows($result) > 0) {
+                
+                
+                
+                echo "Login Successful";
+            } else {
+                
+                echo "Login Failed";
+            }
+    
+    
+            
 	
 	?>
 </body>
